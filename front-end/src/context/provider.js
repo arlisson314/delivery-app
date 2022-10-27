6738eef3a701f624cf73198ba5c9bd28564f88a1
Leonan Costa <leonanfecosta@gmail.com>
2022-10-27 16:37:26 -0300

    feat: cria o context

---
+++ front-end/src/context/provider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Context from './context';
+
+export default function Provider({ children }) {
+  // const contextValue = {};
+
+  return <Context.Provider value={ contextValue }>{children}</Context.Provider>;
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
