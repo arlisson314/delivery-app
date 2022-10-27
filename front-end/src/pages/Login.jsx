375d59ba0d4df6762f1f2484cdc631af07edee2c
AllysonABedin <98184553+AllysonABedin@users.noreply.github.com>
2022-10-27 17:12:05 -0300

    feat: data-testid no login

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="" method="post">
+        <label htmlFor="input-login">Login
+          <input type="email" name="input-login" data-testid="common_login__input-email" id="input-login" />
+        </label>
+        <label htmlFor="input-password">Senha
+          <input type="password" name="input-password" data-testid="common_login__input-password" id="input-login" />
+        </label>
+        <button type="submit" data-testid="common_login__button-login">LOGIN</button>
+        <button id="register-button" data-testid="common_login__button-register">Ainda não tenho conta</button>
+      </form>
+      <div data-testid="common_login__element-invalid-email"></div>
+    </div>
+  );
+}
