722f5b92b98ebc09fbc58a84d35bd54a37b37b48
Leonan Costa <leonanfecosta@gmail.com>
2022-10-27 19:25:32 -0300

    feat: faz as validações para criação de usuário e comunicação com o backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CustomerProducts from './pages/CustomerProducts';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/products" element={ <CustomerProducts /> } />
     </Routes>
   );
 }
--- front-end/src/components/register/register.component.jsx
+++ front-end/src/components/register/register.component.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { post } from '../../helpers/requests';
 
 export default function RegisterComponent() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const enableBtn = () => {
+      const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+      const minNameLength = 12;
+      const minPasswordLength = 6;
+      const validation = !(emailRegex.test(email)
+      && name.length > minNameLength
+      && password.length > minPasswordLength);
+
+      setIsBtnDisabled(validation);
+    };
+
+    enableBtn();
+  }, [email, password, name]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await post('/register', { email, password, name });
+
+      navigate('/customer/products');
+    } catch ({ response }) {
+      const { status, data } = response;
+      setErrorMessage(`${status} - ${data.message}`);
+    }
+  };
   return (
     <div>
       <h2>Cadastro</h2>
@@ -13,6 +49,8 @@ export default function RegisterComponent() {
             name="name"
             id="name-input"
             data-testid="common_register__input-name"
+            value={ name }
+            onChange={ ({ target }) => setName(target.value) }
           />
         </label>
 
@@ -23,6 +61,8 @@ export default function RegisterComponent() {
             name="email"
             id="email-input"
             data-testid="common_register__input-email"
+            value={ email }
+            onChange={ ({ target }) => setEmail(target.value) }
           />
         </label>
 
@@ -33,12 +73,25 @@ export default function RegisterComponent() {
             name="password"
             id="password-input"
             data-testid="common_register__input-password"
+            value={ password }
+            onChange={ ({ target }) => setPassword(target.value) }
           />
         </label>
 
-        <button type="button" data-testid="common_register__button-register">
+        <button
+          type="submit"
+          data-testid="common_register__button-register"
+          disabled={ isBtnDisabled }
+          onClick={ handleSubmit }
+        >
           Cadastrar
         </button>
+
+        {errorMessage && (
+          <p data-testid="common_register__element-invalid_register">
+            {errorMessage}
+          </p>
+        )}
       </form>
     </div>
   );
