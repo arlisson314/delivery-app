78f67c912a4c86e4313de7bc7193875357d673e8
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-27 19:45:38 -0500

    feat: implementa componente header

---
+++ front-end/src/components/header.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+  return (
+    <div>
+      <button
+        type="submit"
+      >
+        PRODUTOS
+      </button>
+
+      <button
+        type="submit"
+      >
+        MEUS PEDIDOS
+      </button>
+
+      <p>USUARIO</p>
+
+      <button
+        type="submit"
+      >
+        SAIR
+      </button>
+    </div>
+  );
+}
