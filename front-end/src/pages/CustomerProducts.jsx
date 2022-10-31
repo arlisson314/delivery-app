b74c6fbd046bb73102a275d49070fd6a8ace926b
AllysonABedin <98184553+AllysonABedin@users.noreply.github.com>
2022-10-31 17:47:11 -0300

    fix: change endpoint

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -11,7 +11,7 @@ export default function CustomerProducts() {
 
   useEffect(() => {
     const fetchInfos = async () => {
-      const { results } = get('/products');
+      const { results } = get('/customer/products');
       setProducts(results);
     };
     fetchInfos();
