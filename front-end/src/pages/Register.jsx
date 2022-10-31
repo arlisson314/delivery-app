918856d38d4816d5895f4cad3aa58b5a28e58e2f
Leonan Costa <leonanfecosta@gmail.com>
2022-10-31 18:08:50 -0300

    refactor: uso do input generico na pagina de registro

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { post } from '../helpers/requests';
 import GenericBtn from '../components/genericBtn';
+import GenericInput from '../components/genericInput';
 
 export default function Register() {
   const navigate = useNavigate();
@@ -45,42 +46,30 @@ export default function Register() {
       <h2>Cadastro</h2>
 
       <form>
-        <label htmlFor="name-input">
-          Nome
-          <input
-            type="text"
-            name="name"
-            id="name-input"
-            data-testid="common_register__input-name"
-            value={ name }
-            onChange={ ({ target }) => setName(target.value) }
-          />
-        </label>
-
-        <label htmlFor="email-input">
-          Email
-          <input
-            type="email"
-            name="email"
-            id="email-input"
-            data-testid="common_register__input-email"
-            value={ email }
-            onChange={ ({ target }) => setEmail(target.value) }
-          />
-        </label>
-
-        <label htmlFor="password-input">
-          Senha
-          <input
-            type="password"
-            name="password"
-            id="password-input"
-            data-testid="common_register__input-password"
-            value={ password }
-            onChange={ ({ target }) => setPassword(target.value) }
-          />
-        </label>
-
+        <GenericInput
+          label="Nome"
+          name="name"
+          type="text"
+          value={ name }
+          onChange={ ({ target }) => setName(target.value) }
+          testId="common_register__input-name"
+        />
+        <GenericInput
+          label="Email"
+          name="email"
+          type="email"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+          testId="common_register__input-email"
+        />
+        <GenericInput
+          label="Senha"
+          name="password"
+          type="password"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+          testId="common_register__input-password"
+        />
         <GenericBtn
           type="submit"
           dataTestId="common_register__button-register"
@@ -88,7 +77,6 @@ export default function Register() {
           onClick={ handleSubmit }
           name="Cadastrar"
         />
-
         {errorMessage && (
           <p data-testid="common_register__element-invalid_register">
             {errorMessage}
