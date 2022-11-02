86bdb2a167e8972f06a4e0856f045e6c192e9510
Arlisson N. Costa <arlisson314@gmail.com>
2022-11-02 02:04:13 -0500

    feat: adiciona a rota /customer/orders/:idVenda

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import Checkout from './pages/Checkout';
+import CustomerCheckout from './pages/CustomerCheckout';
 import Register from './pages/Register';
 import CustomerProducts from './pages/CustomerProducts';
+import CustomerOrders from './pages/CustomerOrders';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <Login /> } />
-      <Route path="/customer/checkout" element={ <Checkout /> } />
+      <Route path="/customer/checkout" element={ <CustomerCheckout /> } />
+      <Route path="/customer/orders/:idVenda" element={ <CustomerOrders /> } />
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <CustomerProducts /> } />
     </Routes>
